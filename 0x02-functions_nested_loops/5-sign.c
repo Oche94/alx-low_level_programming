edaf5f5149f2ce762416b0823d983f2841fc1824
oche94 <o.alogwu@gmail.com>
2023-02-22 11:20:11 -0800

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,23 +6,24 @@
  *
  * @n: function parameter
  *
- * Return: 1, 0, -1
+ * Return: 1, 0 and -1
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+, ");
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		printf("0, ");
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		printf("-, ");
+		_putchar('-');
 		return (-1);
 	}
 }
